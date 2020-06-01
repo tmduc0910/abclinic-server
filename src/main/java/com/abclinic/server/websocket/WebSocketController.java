1c46883fd86c680e92d86aeaa82cf380d0579dcb
tmduc0910 <tmduc0910@gmail.com>
2020-06-01 09:38:53 +0700

    Update

--- src/main/java/com/abclinic/server/websocket/WebSocketController.java
+++ src/main/java/com/abclinic/server/websocket/WebSocketController.java
@@ -3,15 +3,11 @@ package com.abclinic.server.websocket;
 import com.abclinic.server.model.dto.NotificationDto;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.annotation.SubscribeMapping;
-import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
-import sun.plugin2.message.Message;
 
 /**
  * @author tmduc
