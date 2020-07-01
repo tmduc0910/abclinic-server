b5bef7f0bef5786360d9942696a40ef583096399
tmduc0910 <tmduc0910@gmail.com>
2020-07-01 10:12:11 +0700

    Update AOP

--- src/main/java/com/abclinic/server/annotation/authorized/AuthAspect.java
+++ src/main/java/com/abclinic/server/annotation/authorized/AuthAspect.java
@@ -1,17 +1,24 @@
 package com.abclinic.server.annotation.authorized;
 
+import com.abclinic.server.common.base.CustomRuntimeException;
 import com.abclinic.server.exception.ForbiddenException;
 import com.abclinic.server.model.entity.user.User;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.reflections.Reflections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.jdo.annotations.Join;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author tmduc
@@ -21,12 +28,16 @@ import java.util.Arrays;
 @Aspect
 @Component
 public class AuthAspect {
-    @Around("execution(* *.*(..)) && @annotation(annotation)")
-    public Object execute(ProceedingJoinPoint joinPoint, Restricted annotation) throws Throwable {
+    @Before("@annotation(annotation)")
+    public void execute(Restricted annotation) throws ForbiddenException {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
         User user = (User) request.getAttribute("User");
-        if (Arrays.asList(annotation.included()).contains(user.getClass()) || !Arrays.asList(annotation.excluded()).contains(user.getClass()))
-            return joinPoint.proceed();
-        throw new ForbiddenException(user.getId(), "Không có quyền hạn sử dụng API này");
+
+        //Nếu có định nghĩa included và user không thuộc include
+        //hoặc nếu có định nghĩa excluded và user thuộc excluded
+        //trả về 403 Forbidden và chặn API
+        if ((annotation.included().length != 0 && !Arrays.asList(annotation.included()).contains(user.getClass()) ||
+                (annotation.excluded().length != 0 && Arrays.asList(annotation.excluded()).contains(user.getClass()))))
+            throw new ForbiddenException(user.getId(), "Không có quyền hạn sử dụng API này");
     }
 }
